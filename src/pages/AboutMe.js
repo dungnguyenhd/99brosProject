e4a709ac333457012377dce4cc7242373207c14b
nguyentienlam14 <lamcloudy99@gmail.com>
2022-07-05 14:47:03 +0700

    gioi thieu chua hoan thien =)))))

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,16 +1,32 @@
 import React from 'react';
 
+
 const NoPage = () => {
   return (
     <>
-    <div style={{paddingTop: "7rem",}} >
-      <h1>Name: DungNguyen</h1>
-      <h1>Email: dungnguyent9902@gmail.com</h1>
-      <h1>Donate me: =))))))) </h1>
-      </div>
+      <div style={{paddingTop: "7rem",}} class = "container fluid">
+        <div class="col-6 text-justify " >
+          <h1 style={{lineHeight: "70px"}}>Về 99BROS</h1>
+          <p class = "blockquote">
+            99bros là công ty cung cấp dịch vụ thuê văn phòng chuyên nghiệp tại Hà Nội và TP. Hồ Chí Minh.
+            Chúng tôi trợ giúp khách hàng doanh nghiệp tìm thuê văn phòng tại các tòa nhà hạng A,B,C nhanh chóng,
+            tối ưu chi phí và phù hợp mục đích sử dụng!
+          </p>
+        </div>
 
+        <div>
+          <h4>Dịch vụ tư vấn hoàn toàn miễn phí, nhận ngay:</h4>
+        </div>
+        <div>
+          <ul>
+            <li>Bản đề xuất văn phòng nhiều lựa chọn nhất chỉ sau một cuộc gọi</li>
+            <li>Báo cáo so sánh chi tiết tất cả thông tin giá, phí và chính sách thuê</li>
+            <li>Thông tin ưu & Nhược điểm của từng khu vực , từng toà nhà</li>
+          </ul>
+        </div>
+      </div>
     </>
-  );
+  )
 };
 
 export default NoPage;
